4bab73c3f81017b859d19b77390d7f61ed451337
Abdus Sami <abdussamijawed@gmail.com>
2024-07-19 01:06:57 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const name = "Username";
+alert('Username')
+// task2
+var message;
+message = "Hello World";
+alert(message)
+// task3
+var firstname = "Jhone";;
+var lastname = "Doe";
+alert(firstname + lastname);
+var age = "15 years old";
+alert(age);
+var hobby = "Certified  Mobile App Development";
+alert(hobby);
+// task4
+var pizza = "pizza \n pizz \n piz \n pi \n p";
+alert(pizza);
+//task5
+var email = "example@example.com"
+alert('My email address is '+ email)
+//task6
+var  book = "A smarter way to learn JavaScript"
+alert('I am trying to learn from ' + book)
+//task7
+var logo = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(logo)
